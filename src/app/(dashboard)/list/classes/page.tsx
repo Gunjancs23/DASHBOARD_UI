abb9645fa2c8c757836180bfaec6c93c61c69e5d
Gunjancs23 <gunjansharmabaruah@gmail.com>
2026-05-26 03:05:31 +0530

    fix: null supervisor in classes page

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -14,7 +14,7 @@ import {
 import { Class, Prisma, Teacher } from "@prisma/client";
 import { auth } from "@clerk/nextjs/server";
 
-type ClassList = Class & { supervisor: Teacher };
+type ClassList = Class & { supervisor: Teacher | null };
 
 const ClassListPage = async ({
   searchParams,
@@ -65,7 +65,9 @@ const renderRow = (item: ClassList) => (
     <td className="hidden md:table-cell">{item.capacity}</td>
     <td className="hidden md:table-cell">{item.name[0]}</td>
     <td className="hidden md:table-cell">
-      {item.supervisor.name + " " + item.supervisor.surname}
+      {item.supervisor
+        ? item.supervisor.name + " " + item.supervisor.surname
+        : "N/A"}
     </td>
     <td>
       <div className="flex items-center gap-2">
@@ -83,8 +85,6 @@ const renderRow = (item: ClassList) => (
   const { page, ...queryParams } = searchParams;
 
   const p = getPageNumber(page);
-
-  // URL PARAMS CONDITION
 
   const query: Prisma.ClassWhereInput = {};
 
@@ -168,7 +168,6 @@ const renderRow = (item: ClassList) => (
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* TOP */}
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
@@ -180,9 +179,7 @@ const renderRow = (item: ClassList) => (
           </div>
         </div>
       </div>
-      {/* LIST */}
       <Table columns={columns} renderRow={renderRow} data={data} />
-      {/* PAGINATION */}
       <Pagination page={p} count={count} />
     </div>
   );
